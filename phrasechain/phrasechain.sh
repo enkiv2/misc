c4caf78d1073092a9252c01cf786f5ad2c66580c
enkiv2 <john.ohno@gmail.com>
2016-10-17 09:32:36 -0400

    reformat, add documentation, and change paragraph splitting behavior so that we don't lose context between paragraphs

--- phrasechain/phrasechain.sh
+++ phrasechain/phrasechain.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env zsh
-sed 's/^      /<br>./;s/^  *//;s/  *$//' | tr '\n.' ' \n' | sed 's/^  *//' | ./phrasechain.py "$@" | tr '\n' '.' | sed 's/<br>\./\n\n/g;s/\./. /g'
+sed 's/^      / <br> /;s/^	/ <br> /;s/^$/ <br> /;s/^  *//;s/  *$//' | 
+	tr '\n.' ' \n' | sed 's/^  *//' | 		# Remove most fmt-style formatting in favor of one sentence (defined by terminating period) per line, with paragraph breaks substituted with '<br>'
+	./phrasechain.py "$@" | 
+	tr '\n' '.' | sed 's/<br> */\n\n/g;s/\./. /g' |	# replace newlines with periods again and turn paragraph breaks into newlines
+	grep -v '^\.$'					# remove empty lines
 
