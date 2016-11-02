5d570ba34aa9064519a648fcb6aa70e1d7b0e04e
enkiv2 <john.ohno@gmail.com>
2016-11-02 09:19:11 -0400

    use pronouncingpy instead of nltk

---
+++ nanogenmo-2016/rhyme-dict-sample3.md
@@ -0,0 +1,34 @@
+## C
+
+CUL DE SAC: giveback
+
+## H
+
+HORS D'OEUVRE: curve
+
+## I
+
+I P: whack (via t a c)
+
+## L
+
+LA CROSSE: gloss
+
+## N
+
+NEW HAMPSHIRITE: leukocyte
+
+NEW MEXICO'S: overthrows
+
+## T
+
+T A C: snee
+
+## U
+
+U L: befell
+
+## V
+
+VAN GOGH: toe
+
--- nanogenmo-2016/rhyme-dict.py
+++ nanogenmo-2016/rhyme-dict.py
@@ -2,54 +2,23 @@
 # -*- coding: utf-8
 import os, sys, re
 
-import nltk
-from nltk.corpus import wordnet
-from nltk.corpus import cmudict
+import pronouncing
 
 from random import Random
 
-global rhymes, rhyme_chunks, random, rhymelevel, antonyms, synonyms, hypernyms, hyponyms
+global rhymes
 
 random=Random()
 
-antonyms={}
-synonyms={}
-hypernyms={}
-hyponyms={}
 rhymes={}
-rhyme_chunks={}
-rhymelevel=2
 
 
-# from http://kashthealien.wordpress.com/2013/06/15/213/
-def rhyme(inp, level):
-	global rhyme_chunks
-	if(not inp.isalpha()):
-		return []
-	inp=inp.lower()
-	key=""
-	entries=cmudict.entries()
-	syllables = [(word, syl) for word, syl in entries if word == inp]
-	if(len(syllables)>0):
-		key=repr(syllables[0][1][-level:])
-		if(key in rhyme_chunks):
-			#print("Skipping because "+inp+" has a memoized rhyme")
-			return rhyme_chunks[key]
-	else:
-		#print("Skipping because "+inp+" has no rhymes")
-		return []
-	myRhymes = []
-	for (word, syllable) in syllables:
-		myRhymes += [word for word, pron in entries if pron[-level:] == syllable[-level:]]
-	if(len(syllables)>0): 
-		rhyme_chunks[key]=list(set(myRhymes))
-	return list(set(myRhymes))
 
 def randomRhyme(w):
 	global rhymes, random, rhymelevel
 	w=w.lower()
 	if not (w in rhymes):
-		rhymes[w]=rhyme(w, rhymelevel)
+		rhymes[w]=pronouncing.rhymes(w)
 		rhymes[w].append(w)
 	if len(rhymes[w])==0: return w
 	return random.choice(rhymes[w])
@@ -58,7 +27,7 @@ def multiWordRhymesOnly(w):
 	global rhymes
 	w=w.lower()
 	if not (w in rhymes):
-		rhymes1=rhyme(w, rhymelevel)
+		rhymes1=pronouncing.rhymes(w)
 		rhymes1.append(w)
 		rhymes[w]=[]
 		for piece in rhymes1:
@@ -70,26 +39,23 @@ def multiWordRhymesOnly(w):
 
 def main():
 	i=0
+	skip=[]
 	rhymeDict={}
 	for line in sys.stdin.readlines():
 		for word in re.split(r'([A-Za-z0-9]+|[^A-Za-z0-9]+)', line):
 			rhym=multiWordRhymesOnly(word)
 			if(rhym!=word):
 				rhymeDict[rhym]=word
+				w2=multiWordRhymesOnly(rhym.split()[0]).replace("_", " ")
+				if(rhym.find(w2)!=0 and w2 not in rhymeDict and w2 not in skip):
+					print(w2.upper()+": "+rhymeDict[rhym]+" (via "+rhym+")\n")
+					skip.append(w2)
+				else:
+					print(rhym.upper()+": "+rhymeDict[rhym]+"\n")
 				i+=1
 				if(i>25000): 
 					break
 		if(i>25000): 
 			break
-	skip=[]
-	k=rhymeDict.keys()
-	k.sort()
-	for w in k:
-		w2=multiWordRhymesOnly(w.split()[0]).replace("_", " ")
-		if(w.find(w2)!=0 and w2 not in k and w2 not in skip):
-			print(w2.upper()+": "+rhymeDict[w]+" (via "+w+")\n")
-			skip.append(w2)
-		else:
-			print(w.upper()+": "+rhymeDict[w]+"\n")
 	
 if __name__=="__main__": main()
