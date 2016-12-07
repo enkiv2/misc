6ae3e51d07509d9a40fc266e11cd52552683a0fc
enkiv2 <john.ohno@gmail.com>
2016-12-07 16:39:29 -0500

    change this to produce CBDQ tracery code for @memebombbot

--- kwotes.py
+++ kwotes.py
@@ -1,13 +1,22 @@
+#!/usr/bin/env python
+
 from pyquery import PyQuery
+import json
+
+global res
+res=[]
 
 def grabber(i=0):
+    global res
     base_url = "http://principiadiscordia.com/memebombs/"
     url = base_url + "kwotes.pl?action=list&m=501&so=reverse&o=date&s=" + str(i)
     PQ = PyQuery(url)
     BQ = PQ("blockquote")
     for x in BQ:
         try:
-           print x.text
+           text=x.text
+           text=text.replace("\r", "").strip()
+           res.append(text)
         except:
             error = 1
             #this is not error handling... it's error ignoring.
@@ -16,3 +25,4 @@ def grabber(i=0):
         grabber(i+500)
 
 grabber()
+print(json.dumps({"origin":res}))
