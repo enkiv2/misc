769ae921fb2c6136cf8758ca1301e3da0aed9841
enkiv2 <john.ohno@gmail.com>
2017-02-22 16:48:55 -0500

    xxx

---
+++ notes.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env zsh
+
+[[ -e ~/.notes ]] || touch ~/.notes
+
+function addnote() {
+	read x 
+	echo "$x" >> ~/.notes
+}
+function rnote() {
+	if [[ $# -eq 0 ]] ; then
+		shuf -n 1 ~/.notes
+	else
+		egrep "$@" ~/.notes | shuf -n 1
+	fi
+}
+function gnote() {
+	egrep "$@" ~/.notes
+}
+function lnote() {
+	if [[ $# -eq 0 ]] ; then
+		tail -n 1 ~/.notes
+	else
+		tail -n "$@" ~/.notes
+	fi
+}
+
