55559c5a22448c80bb14684e5cb3d904a2fae752
enkiv2 <john.ohno@gmail.com>
2017-03-20 17:50:20 -0400

    fixes

--- X_except_its_Y.py
+++ X_except_its_Y.py
@@ -1,17 +1,21 @@
 #!/usr/bin/env python
-from gensim import Word2Vec
+import gensim
 from gensim.models import word2vec
 
+from nltk.corpus import webtext
 import sys
 
 try:
 	open("text8.wv", 'r')
-	model=Word2Vec.load("text8.wv")
+	model=gensim.models.Word2Vec.load("text8.wv")
 except:
 	sys.stderr.write("Corpus not found; training...")
 	sys.stderr.flush()
-	sentences = word2vec.Text8Corpus('text8')
-	model=Word2Vec(sentences)
+	sentences=[]
+	for s in webtext.paras():
+		for j in s:
+			sentences.append(j)
+	model=gensim.models.Word2Vec(sentences)
 	model.save("text8.wv")
 	sys.stderr.write("\ttrained!\n")
 	sys.stderr.flush()
@@ -20,7 +24,7 @@ def doubleSplit(fname):
 	ret=[]
 	f=open(fname, 'r')
 	for line in f.readlines():
-		ret.push(line.split())
+		ret.append(line.split())
 	return ret
 
 corpus1=doubleSplit(sys.argv[1])
@@ -33,8 +37,13 @@ for l in range(0, len(corpus1)):
 			l2=l%len(corpus2)
 		w2=w
 		if(w2>=len(corpus2[l2])):
+			if(len(corpus2[l2])<1):
+				corpus2[l2]=[""]
 			w2=w%len(corpus2[l2])
-		sys.stdout.write(model.wv.most_similar(positive=[ corpus1[l][w], corpus2[l2][w2] ])[0][0])
+		try:
+			sys.stdout.write(model.most_similar(positive=[ corpus1[l][w], corpus2[l2][w2] ])[0][0])
+		except:
+			sys.stdout.write(corpus1[l][w])
 		sys.stdout.write(" ")
 		sys.stdout.flush()
 	sys.stdout.write("\n")
