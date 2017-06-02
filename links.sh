0ea5b015be7411a545925f648bcc215f31e05791
enkiv2 <john.ohno@gmail.com>
2017-06-02 12:47:04 -0400

    handle titles when possible

--- links.sh
+++ links.sh
@@ -2,7 +2,10 @@
 [[ -e ~/.linkit ]] || touch ~/.linkit
 
 function linkit() {
-	echo "$1\t$(date)" >> ~/.linkit
+	echo "$1\t$(date)\t$(getTitle "$1")" >> ~/.linkit
+}
+function getTitle() {
+	curl "$1"| grep -i "<title>" | head -n 1 | sed 's/^.*<[tT][iI][tT][lL][eE]>//;s/<\/[tT][iI][tT][lL][eE]>.*//' 
 }
 function fmtlinks() {
 	echo "<html>"
@@ -10,7 +13,7 @@ function fmtlinks() {
 	echo "<body>"
 	echo "<table>"
 	echo "<tr><th>Link</th><th>Date</th></tr>"
-	tac ~/.linkit| awk 'BEGIN{FS="\t"} {print "<tr><td><a href=\"" $1 "\">" $1 "</a></td><td>" $2 "</td></tr>"}'
+	tac ~/.linkit| awk 'BEGIN{FS="\t"} {title=$1; if($3) title=$3; print "<tr><td><a href=\"" $1 "\">" title "</a></td><td>" $2 "</td></tr>"}'
 	echo "</table>"
 	echo "<center><a href=\"https://github.com/enkiv2/misc/blob/master/links.sh\">Generated with links.sh</a></center>"
 	echo "</body>"
