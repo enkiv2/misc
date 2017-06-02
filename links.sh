efdb1238d37166e393b44b2077cb3c7af3f53b60
enkiv2 <john.ohno@gmail.com>
2017-06-02 12:34:29 -0400

    fix formatting & add link to generator

--- links.sh
+++ links.sh
@@ -12,7 +12,9 @@ function fmtlinks() {
 	echo "<tr><th>Link</th><th>Date</th></tr>"
 	tac ~/.linkit| awk 'BEGIN{FS="\t"} {print "<tr><td><a href=\"" $1 "\">" $1 "</a></td><td>" $2 "</td></tr>"}'
 	echo "</table>"
+	echo "<center><a href=\"https://github.com/enkiv2/misc/blob/master/links.sh\">Generated with links.sh</a></center>"
 	echo "</body>"
+	echo "</html>"
 }
 function uploadlinks() {
 	fmtlinks > ~/index.html
