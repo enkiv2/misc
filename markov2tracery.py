49f03451affbf341fc5fd86f140d74cc2e444589
John Ohno <john.ohno@gmail.com>
2017-09-08 11:34:04 -0400

    omit blank spaces; fix indentation

--- markov2tracery.py
+++ markov2tracery.py
@@ -4,26 +4,27 @@ import json
 import re
 
 def markov2tracery(s):
-	grammar={}
-	last="origin"
-	
-	words=s.split()
-	for w in words:
-		word=re.sub(r"\W+", "", w).lower()
-		key=w+" #"+word+"#"
-		if not (last in grammar):
-			grammar[last]=[key]
-		else:
-			grammar[last].append(key)
-		last=word
-	if not (last in grammar):
-		grammar[last]=[""]
-	else:
-		grammar[last].append("")
-	return json.dumps(grammar)
+    grammar={}
+    last="origin"
+    
+    words=s.split()
+    for w in words:
+        if(w!=""):
+            word=re.sub(r"\W+", "", w).lower()
+            key=w+" #"+word+"#"
+            if not (last in grammar):
+                    grammar[last]=[key]
+            else:
+                    grammar[last].append(key)
+            last=word
+    if not (last in grammar):
+        grammar[last]=[""]
+    else:
+        grammar[last].append("")
+    return json.dumps(grammar)
 
 if __name__=="__main__":
-	import sys
-	for line in sys.stdin.readlines():
-		print(markov2tracery(line))
+    import sys
+    for line in sys.stdin.readlines():
+        print(markov2tracery(line.strip()))
 
