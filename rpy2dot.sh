c3152aaa5ecf078fd454a36f528eb102778dc343
John Ohno <john.ohno@gmail.com>
2017-11-21 12:14:39 -0500

    handle fall-through properly; remove duplicate paths; support parsing only a subset of rpy files

--- rpy2dot.sh
+++ rpy2dot.sh
@@ -1,27 +1,32 @@
 #!/usr/bin/env zsh
-cat *.rpy | 
-	sed 's/^  *//g' | 
-	egrep '^(label|jump)' | 
-	tr -d ':\r' | 
-	grep -v 'label _' | awk '
-		/^label / { 
-			curr_label=$2
-		} 
-		/^jump/ { 
-			print curr_label " -> " $2 ";" 
-		}' | awk '
-			
-		BEGIN { 
-			print "digraph path {" 
-		} 
-		{
-			if(x[$1]) 	x[$1]=x[$1] $0
-			else 		x[$1]=$0
-		}
-		END {
-			print x["start"]
-			for (i in x)
-				if(i!="start")
-					print x[i]
-			print "}"
-		}' 
+if [ $# -lt 1 ] ; then set *.rpy ; fi
+for item in "$@" ; do
+	cat "$item"	| 
+		sed 's/^  *//g' | 
+		egrep '^(label|jump)' | egrep -v '[\."]' |
+		tr -d ':\r' |
+		grep -v 'label _' | awk '
+			/^label / { 
+				if(curr_label)
+					print curr_label " -> " $2 ";"
+				curr_label=$2
+			} 
+			/^jump/ { 
+				print curr_label " -> " $2 ";" 
+			}' | sort | uniq
+done | awk '
+		
+	BEGIN { 
+		print "digraph path {" 
+	} 
+	{
+		if(x[$1]) 	x[$1]=x[$1] $0
+		else 		x[$1]=$0
+	}
+	END {
+		print x["start"]
+		for (i in x)
+			if(i!="start")
+				print x[i]
+		print "}"
+	}' 
