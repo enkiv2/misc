3e684c2e55837260d5075cc9d502c36e4cd23398
John Ohno <john.ohno@gmail.com>
2017-11-27 12:39:36 -0500

    always stream sourcedocs instead of keeping them in memory

--- minimal-translit/minimalTranslit.py
+++ minimal-translit/minimalTranslit.py
@@ -9,17 +9,18 @@ import json
 
 def ipfsGet(h):
     (stdin, stdout) = os.popen2(["ipfs", "cat", h])
+    stdin.close()
     return stdout
 
-def rcSpan2str(row, col, dRows, dCols, content):
-    lines=content.split("\n")
-    if(dRows==0):
-        return content[row][col:col+dCols]
+def rcSpan2str(row, col, dRows, dCols, contentStream):
     ret=[]
-    ret.append(lines[row][col:])
-    for r in range(1, dRows-1):
-        ret.append(lines[row+r])
-    ret.append(lines[row+dRows][:dCols]
+    for i in range(0, row):
+        line=contentStream.readline()
+    ret.append(line[col:])
+    for i in range(0, dRows-1):
+        ret.append(contentStream.readline())
+    ret.append(contentStream.readline()[:dCols])
+    contentStream.close()
     return "\n".join(ret)
 
 def edl2concatext(edl):
