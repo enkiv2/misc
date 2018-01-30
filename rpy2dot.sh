464dac72e73d192212ae46aaaf6733a8a937c39b
John Ohno <john.ohno@gmail.com>
2018-01-30 12:21:11 -0500

    cluster by file

--- rpy2dot.sh
+++ rpy2dot.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env zsh
 if [ $# -lt 1 ] ; then set *.rpy ; fi
 for item in "$@" ; do
+	echo "subgraph $(echo $item | sed 's/\.rpy$//')"
 	cat "$item"	| 
 		sed 's/^  *//g' | 
 		egrep '^(label|jump)' | egrep -v '[\."]' |
@@ -19,14 +20,34 @@ done | awk '
 	BEGIN { 
 		print "digraph path {" 
 	} 
+	/^subgraph /{
+		curr_subgraph=$2
+		next
+	}
 	{
 		if(x[$1]) 	x[$1]=x[$1] $0
-		else 		x[$1]=$0
+		else {
+			x[$1]=$0
+			c[$1]=curr_subgraph
+		}
 	}
 	END {
+		print "subgraph cluster_" c["start"] " {"
 		print x["start"]
+		print "style=\"rounded,dashed\";"
+		print "color=blue;"
+		print "labelalloc=b;"
+		print "label=\"" c["start"] "\";"
+		print "}"
 		for (i in x)
-			if(i!="start")
+			if(i!="start") {
+				print "subgraph cluster_" c[i] "{"
 				print x[i]
+				print "style=\"rounded,dashed\";"
+				print "color=blue;"
+				print "labelalloc=b;"
+				print "label=\"" c[i] "\";"
+				print "}"
+			}
 		print "}"
 	}' 
