b83af7ecf45f810ddbc75ee037263fae781d8403
John Ohno <john.ohno@gmail.com>
2018-05-31 12:54:05 -0400

    debug some stupid golang-noob errors

--- kamui/senketsu.go
+++ kamui/senketsu.go
@@ -1,28 +1,40 @@
+package senketsu
 // senketsu - a dialect of io with a persistent memory-mapped lobby & a MUMPS-style global persistent list-keyed database
-struct object {
+
+import (
+//	"strconv"
+	"fmt"
+)
+
+type object struct {
 	typeName string
 	name	string
 	parent *object
 	supportedMessages map[string]string
 	mailbox chan message
 }
-struct message {
+type message struct {
 	messageName string
-	args object[]
+	args []object
 	response chan object
 	expectsResponse bool
 }
 
-var Object object {"Object", "Object", nil, nil, make(chan message)}
-var Future object {"Future", "Future", &Object, nil, make(chan message)}
+var Object object
+var Future object
 
-func (self object) clone(newName string) {
+func initializeEnvironment() {
+	Object := object{"Object", "Object", nil, nil, make(chan message)}
+	Future := object{"Future", "Future", &Object, nil, make(chan message)}
+}
+
+func (self object) clone(newName string) object {
 	var ret=object{}
-	if (newName==nil) {
+	if (newName=="") {
 		ret.typeName=self.typeName
-		newName=self.typeName+&ret
+		newName=self.typeName+fmt.Sprintf("_%x", &ret)
 	} else {
-		if(newName[0]>=A && newName[0]<=Z) {
+		if(newName[0]>='A' && newName[0]<='Z') {
 			ret.typeName=newName
 		} else {
 			ret.typeName=self.typeName
@@ -33,27 +45,28 @@ func (self object) clone(newName string) {
 	ret.run()
 	return ret
 }
-func (self object) handle(m message) {
+func (self object) handle(m message) object {
 	// TODO implement error stack and error condition
 	var impl string
 	if(self.supportedMessages != nil) {
 		impl=self.supportedMessages[m.messageName]
 	} else {
-		impl=nil
+		impl=""
 	}
 	var ret object
-	if(impl) {
+	if(impl!="") {
 		// TODO call out to parser/executor, set ret
 	} else {
 		if(self.parent!=nil) {
-			ret=(*(self.parent).handle(m))
+			ret=self.parent.handle(m)
 		} else {
 			// TODO throw error
 		}
 	}
 	if(m.expectsResponse) {
-		ret -> m.response
+		m.response<-ret
 	}
+	return ret
 }
 func (self object) run() {
 	self.mailbox=make(chan message)
@@ -62,21 +75,21 @@ func (self object) run() {
 		case msg := <-self.mailbox:
 			self.handle(msg)
 		}
-	}
+	}()
 }
 func (self object) call(other object, msg message) object{
-	msg -> other.mailbox
+	other.mailbox <- msg
 	var ret object
 	if(msg.expectsResponse) {
-		ret <- msg.response
+		ret := <-msg.response
 		return ret
 	}
-	return nil // TODO: replace with nil object
+	return ret // TODO: replace with nil object
 }
 func (self object) callAsync(other object, msg message) object {
-	var ret=Future.clone(nil)
+	var ret=Future.clone("")
 	go func() {
-		ret=call(other, msg)
-	}
+		ret=self.call(other, msg)
+	}()
 	return ret
 }
