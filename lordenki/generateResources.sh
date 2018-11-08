48b17de30f37a3e3679ff06e4481ad811319fd84
John Ohno <john.ohno@gmail.com>
2018-11-08 18:06:23 -0500

    Generate 'family tree' of GUIs; add FORTH links; move things around and rename them in the resources

--- lordenki/generateResources.sh
+++ lordenki/generateResources.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env zsh
 . ../links.sh
-(echo "<html><head><title>Resources for A Survey Of Alternatives</title><link rel="stylesheet" type="text/css" href="vt240.css"></head><body><ul>" ; cat resources-survey-of-alternatives.txt | while read x ; do if echo "$x" | grep -q '://' ; then echo "<li><a href=\"$x\">$(y=$(getTitle "$x") ; [ -z "$y" ] && echo $x || echo $y )</a></li>" ; else echo "</ul>$x<ul>" ; fi ; done ; echo "</ul></body></html>") | tee resources-survey-of-alternatives.html
+dot < gui_evolution.dot -Tpng > gui_evolution.png
+convert gui_evolution.png -colorspace HSI -channel B -level 100,0%  +channel -colorspace sRGB gui_evolution2.png
+scp gui_evolution2.png accela_lordenki@ssh.phx.nearlyfreespeech.net:/home/public/
+(echo "<html><head><title>Resources for A Survey Of Alternatives</title><link rel="stylesheet" type="text/css" href="vt240.css"></head><body><ul>" ; cat resources-survey-of-alternatives.txt | while read x ; do if echo "$x" | grep -q '://' ; then echo "<li><a href=\"$x\">$(y=$(getTitle "$x") ; [ -z "$y" ] && echo $x || echo $y )</a></li>" ; else echo "</ul>$x<ul>" ; fi ; done ; echo "<li><a href=\"gui_evolution2.png\">Map of GUI evolution</a></li></ul></body></html>") | tee resources-survey-of-alternatives.html
 scp resources-survey-of-alternatives.html accela_lordenki@ssh.phx.nearlyfreespeech.net:/home/public
 
