98c58dd0a75a89bfc15b1de2f4db6f1e63e795d0
John Ohno <john.ohno@gmail.com>
2018-11-12 12:11:18 -0500

    skip blank & already-present links

--- links.sh
+++ links.sh
@@ -2,12 +2,15 @@
 [[ -e ~/.linkit ]] || touch ~/.linkit
 
 function linkit() {
-	(which mass_archive 2>&1 > /dev/null && mass_archive "$1")
-	export LC_ALL=en_US.UTF-8
-	echo -e "$1\t$(date)\t$(getTitle "$1")" >> ~/.linkit
-	post="$(export LC_ALL=en_US.UTF-8 ; tail -n 1 ~/.linkit | awk 'BEGIN{FS="\t"} {url=$1 ; title=$3 ; if(url!=title && title!=""&&title!=" ") {if(length(url)+length(title)>=280) {delta=(length(url)+length(title))-280; delta+=4; if(delta<length(title)) { title=substr(title, 0, length(title)-delta) "..." ; print title " " url } } else print title " " url } }' | grep -a . | head -n 1 | recode -f UTF8)"
-	[[ -n "$post" ]] && post "$post"
-	stty sane
+	[ -z "$1" ] || (
+		grep -n -- "$1" ~/.linkit || (
+			(which mass_archive 2>&1 > /dev/null && mass_archive "$1")
+			export LC_ALL=en_US.UTF-8
+			echo -e "$1\t$(date)\t$(getTitle "$1")" >> ~/.linkit
+			post="$(export LC_ALL=en_US.UTF-8 ; tail -n 1 ~/.linkit | awk 'BEGIN{FS="\t"} {url=$1 ; title=$3 ; if(url!=title && title!=""&&title!=" ") {if(length(url)+length(title)>=280) {delta=(length(url)+length(title))-280; delta+=4; if(delta<length(title)) { title=substr(title, 0, length(title)-delta) "..." ; print title " " url } } else print title " " url } }' | grep -a . | head -n 1 | recode -f UTF8)"
+			[[ -n "$post" ]] && post "$post"
+			stty sane 
+	) )
 }
 function getTitle() {
 	curl -m 2 "$1"| grep -a -i "<title>" | head -n 1 | sed 's/^.*<[tT][iI][tT][lL][eE]>//;s/<\/[tT][iI][tT][lL][eE]>.*//' | 
