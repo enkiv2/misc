772f498349d5411d83447d38c5d1ad309628f871
John Ohno <john.ohno@gmail.com>
2019-01-02 11:22:04 -0500

    mark end states as red & begin states as blue

--- rpy2dot.sh
+++ rpy2dot.sh
@@ -53,4 +53,4 @@ done | awk '
 				print x[i]
 			}
 		print "}"
-	}' | gvpr  -c 'N[degree==1]{color="red"}' # from https://stackoverflow.com/questions/6505982/graphviz-dot-how-to-mark-all-leaves-in-a-tree-with-a-distinctive-color
+	}' | gvpr -c 'N[outdegree==0]{ color="red"}' | gvpr -c 'N[indegree==0]{ color="blue"}' 
