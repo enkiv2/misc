e86b83cccae5df3a3f348db5dd2fcc5ed0b4fc7e
John Ohno <john.ohno@gmail.com>
2019-07-31 07:32:17 -0400

    randlinkd should link to the internet archive version from the date we originally posted the link on

--- randlinkd.sh
+++ randlinkd.sh
@@ -1,5 +1,5 @@
 (while : ; do 
-	post "$(shuf -n 1 ~/.linkit | sed 's/^\([^\t]*\)\t\([^\t]*\).*$/Random link from the archives: \1 originally posted \2/')" 
+	post "$(item="$(shuf -n 1 ~/.linkit)"; date="$(echo "$item" | cut -f 2)"; date2=$(date -d "$date" +"%Y%m%d%H%M%S"); url="$(echo "$item" | cut -f 1)"; echo "Random link from the archives: https://web.archive.org/web/$date2/$url originally posted $date/")" 
 	sleep 4h
 done 2>&1) > /dev/null 
 
