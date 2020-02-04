a66d01384469a2d02aaf0bd1220dd9c4492d772e
John Ohno <john.ohno@gmail.com>
2020-02-04 10:32:45 -0500

    xxx

--- process_medium_exports.sh
+++ process_medium_exports.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env zsh
-for i in medium-export/*.html ; do w3m -dump "$i" ; done  | sed 's/^$/\\r/g' | tr '\n' ' ' | sed 's/\\r/\n/g' | sed 's/^  *//' | egrep -v '^(View original|Canonical link|By A pig|Exported from Medium on|Tagged in)' | egrep -v '(━━━━|…)' | sed 's/\([\t ]\)[\t ]*/\1/g' | sed 's/[ \t]*$//' | grep . |  uniq
+for i in medium-export/*.html ; do w3m -dump "$i" ; done  | sed 's/^$/\\r/g' | tr '\n' ' ' | sed 's/\\r/\n/g' | sed 's/^  *//' | egrep -v '^(View original|Canonical link|By John|Exported from Medium on|Tagged in)' | egrep -v '(━━━━|…)' | sed 's/\([\t ]\)[\t ]*/\1/g' | sed 's/[ \t]*$//' | grep . |  uniq
 
