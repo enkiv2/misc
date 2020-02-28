e10d2be12037a5ee0c668d120265f1d195f43bc0
John Ohno <john.ohno@gmail.com>
2020-02-28 10:07:40 -0500

    xxx

--- ravendesk.py
+++ ravendesk.py
@@ -5,6 +5,8 @@ from tracery.modifiers import base_english
 import pycorpora
 
 rules=tracery.Grammar({"origin":"How is #noun.a# like #noun.a#?", "noun":pycorpora.words.nouns["nouns"]})
+rules=tracery.Grammar({"origin":"I am once again asking you how #noun.a# is like #noun.a#?", "noun":pycorpora.words.nouns["nouns"]})
+
 rules.add_modifiers(base_english)
 
 print(rules.flatten("#origin#"))
