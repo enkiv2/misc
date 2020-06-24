301d7dc631008938fb1eb19b4b8f1cdad61721fc
John Ohno <john.ohno@gmail.com>
2020-06-24 13:31:26 -0400

    post title when possible, and fallback to smaller and smaller templates

--- randlinkd.sh
+++ randlinkd.sh
@@ -1,5 +1,31 @@
+processLink() {
+	read item;
+	date="$(echo "$item" | cut -f 2)" 
+	date2=$(date -d "$date" +"%Y%m%d%H%M%S") 
+	url="$(echo "$item" | cut -f 1)"
+	url2="https://web.archive.org/web/$date2/$url"
+	title="$(echo "$item" | cut -f 3-)"
+	template="Random link from the archives: \"$title\" $url2 originally posted $date"
+	if [ $(echo "$template" | wc -c) -gt 500 ]; then
+		template="Random link from the archives: \"$title\" $url2"
+		if [$(echo "$template" | wc -c) -gt 500 ]; then
+			template="#randomArchiveLink \"$title\" $url2 @ $date"
+			if [$(echo "$template" | wc -c) -gt 500 ]; then
+				template="#randomArchiveLink \"$title\" $url2 @ $date2"
+				if [$(echo "$template" | wc -c) -gt 500 ]; then
+					template="#randomArchiveLink \"$title\" $url2"
+					if [$(echo "$template" | wc -c) -gt 500 ]; then
+						template="#randomArchiveLink $url2 @ $date"
+					fi
+				fi
+			fi
+  	fi
+  fi
+	echo "$template"
+}
 (while : ; do 
-	post "$(item="$(shuf -n 1 ~/.linkit)"; date="$(echo "$item" | cut -f 2)"; date2=$(date -d "$date" +"%Y%m%d%H%M%S"); url="$(echo "$item" | cut -f 1)"; echo "Random link from the archives: https://web.archive.org/web/$date2/$url originally posted $date/")" 
-	sleep 4h
+	post "$(shuf -n 1 ~/.linkit | processLink)" 
+	#sleep 4h
+	sleep 23m
 done 2>&1) > /dev/null 
 
