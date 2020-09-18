d118799f29cbf89220c4f2ced122a87d3f9570e4
John Ohno <john.ohno@gmail.com>
2020-09-18 16:08:40 -0400

    xxx

--- defines.sh
+++ defines.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env zsh
 
-strings | sed "s/[^A-Za-z'][^A-Za-z']*/\n/g" | grep . | sort | uniq -c | sort -n | sed 's/^ *[0-9]* //' | while read x; do w3m -dump "http://en.m.wiktionary.org/wiki/$x" ; sleep 23 ; done
+strings | tr '.' ' ' | sed "s/[^A-Za-z'][^A-Za-z']*/\n/g" | grep . | sort | uniq -c | sort -n | sed 's/^ *[0-9]* //' | while read x; do w3m -dump "http://en.m.wiktionary.org/wiki/$x" ; sleep 23 ; done
 
