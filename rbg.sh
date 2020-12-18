66a234af1b9abd2dc928ff9020a0121bf359c1ec
John Ohno <john.ohno@gmail.com>
2020-12-18 18:25:25 -0500

    xxx

---
+++ rbg.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env zsh
+
+if [[ "$1" == "-h" ]] ; then
+	echo -e "RBG: random background image\nUsage: $0 delay directory [directory ...]" >&2
+	exit 1
+fi
+
+delay=$1 ; shift
+
+while : ; do
+	(for dir in "$@" ; do
+		find -type f $dir
+	done) | shuf | while read x ; do
+		feh --bg-max "$x" && sleep $delay
+	done
+done
+
