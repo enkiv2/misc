694f48172f1281d188470be729e101e162320241
John Ohno <john.ohno@gmail.com>
2022-05-06 21:44:54 -0400

    break different processes out into different functions

--- lordenki/survey-of-alternatives/genlatex.sh
+++ lordenki/survey-of-alternatives/genlatex.sh
@@ -1,63 +1,73 @@
 #!/usr/bin/env zsh
-(
-cat prefix.latex
-cat gloss.txt | sed 's/\(^.*\): \(.*\)$/\\newglossaryentry{\1}{name={\1},description={\2}}/'
 
-echo '\\begin{document}'
-cat frontmatter.latex
+function genLatex() {
+	cat prefix.latex
+	cat gloss.txt | sed 's/\(^.*\): \(.*\)$/\\newglossaryentry{\1}{name={\1},description={\2}}/'
 
-echo '\\mainmatter'
-echo '\\begin{sidewaysfigure}
-	\\includesvg[width=5in]{gui_evolution}
-\\caption{The evolution of user interfaces, showing the flow of ideas between related projects}
-\\end{sidewaysfigure}'
+	echo '\\begin{document}'
+	cat frontmatter.latex
 
-echo "% Main body: chapters"
-for i in intro.txt chapter*.txt ; do 
-	pandoc -f markdown -t latex $i -o $i.latex
-	sed -i 's/\\section{/\\chapter{/;s/\\subsection{/\\section{/' $i.latex
-	sed -i 's/\\hypertarget{.*$//;s/\(\\label{.*}\)}/\1/' $i.latex
-	sed -i 's/\(Big and Small Computing\)/\\textit{\1}\\cite{bigandsmallcomputing}/' $i.latex
-	cut -d: -f 1 < gloss.txt | grep . | while read x ; do
-		sed -i "s/$x/\\\\index{$x}\\\\gls{$x}/g" $i.latex
+	echo '\\mainmatter'
+	echo '\\begin{sidewaysfigure}
+		\\includesvg[width=5in]{gui_evolution}
+	\\caption{The evolution of user interfaces, showing the flow of ideas between related projects}
+	\\end{sidewaysfigure}'
+
+	echo "% Main body: chapters"
+	for i in intro.txt chapter*.txt ; do 
+		pandoc -f markdown -t latex $i -o $i.latex
+		sed -i 's/\\section{/\\chapter{/;s/\\subsection{/\\section{/' $i.latex
+		sed -i 's/\\hypertarget{.*$//;s/\(\\label{.*}\)}/\1/' $i.latex
+		sed -i 's/\(Big and Small Computing\)/\\textit{\1}\\cite{bigandsmallcomputing}/' $i.latex
+		cut -d: -f 1 < gloss.txt | grep . | while read x ; do
+			sed -i "s/$x/\\\\index{$x}\\\\gls{$x}/g" $i.latex
+		done
+		cut -d: -f 1 < gloss.txt | tr 'A-Z' 'a-z' | grep . | while read x ; do
+			sed -i "s/$x/\\\\index{$x}\\\\gls{$x}/g" $i.latex
+		done
+		cat $i.latex 
 	done
-	cut -d: -f 1 < gloss.txt | tr 'A-Z' 'a-z' | grep . | while read x ; do
-		sed -i "s/$x/\\\\index{$x}\\\\gls{$x}/g" $i.latex
-	done
-	cat $i.latex 
-done
 
-echo "% Appendices"
-echo '\\chapter{Appendix A: Resources}' 
-(
-	echo '\\marginnote{The live version of this appendix is available from \\textless \\url{https://www.lord-enki.net/resources-survey-of-alternatives.html} \\textgreater}'
-  first=""
-	cat ../resources-survey-of-alternatives.txt | while read x ; do 
-		if echo "$x" | grep -q '://' ; then
-			echo '\\item'
-			echo '  \\textless \\url{'"$x"'} \\textgreater'
-		else 
-      [ -z "$first" ] || echo '\\end{itemize}'
-      first="no"
-			echo '\\section{'"$x"'}'
-			echo '\\begin{itemize}'
-			echo '\\tightlist'
-		fi
-	done )> resources.latex
-cat resources.latex
-echo '\\chapter{Appendix B: Further Reading}'
-cat further_reading.txt
-cat backmatter.latex
-)> book.latex
+	echo "% Appendices"
+	echo '\\chapter{Appendix A: Resources}' 
+	(
+		echo '\\marginnote{The live version of this appendix is available from \\textless \\url{https://www.lord-enki.net/resources-survey-of-alternatives.html} \\textgreater}'
+		first=""
+		cat ../resources-survey-of-alternatives.txt | while read x ; do 
+			if echo "$x" | grep -q '://' ; then
+				echo '\\item'
+				echo '  \\textless \\url{'"$x"'} \\textgreater'
+			else 
+				[ -z "$first" ] || echo '\\end{itemize}'
+				first="no"
+				echo '\\section{'"$x"'}'
+				echo '\\begin{itemize}'
+				echo '\\tightlist'
+			fi
+		done )> resources.latex
+	cat resources.latex
+	echo '\\chapter{Appendix B: Further Reading}'
+	cat further_reading.txt
+	cat backmatter.latex
+}
 
-echo "R" | pdflatex book.latex 
-biber book
-echo "R" | pdflatex book.latex 
-makeglossaries book
-echo "R" | pdflatex book.latex 
-biber book
-echo "R" | pdflatex book.latex 
-rm -f *.{bbl,blg,glo,idx,ilg,ind,ist,log,run.xml,toc,aux,bcf,glg,gls,lof,lot}
-rm -f *.out
-#rm -f *.latex
-#xpdf book.pdf
+function genPDF() {
+	echo "R" | pdflatex $1.latex 
+	biber $1
+	echo "R" | pdflatex $1.latex
+	makeglossaries $1
+	echo "R" | pdflatex $1.latex 
+	biber $1
+	echo "R" | pdflatex $1.latex 
+}
+
+function cleanup() {
+	rm -f *.{bbl,blg,glo,idx,ilg,ind,ist,log,run.xml,toc,aux,bcf,glg,gls,lof,lot}
+	rm -f *.out
+	#rm -f *.txt.latex resources.latex book.latex
+}
+
+genLatex > book.latex
+genPDF book
+cleanup
+
