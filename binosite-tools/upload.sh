2ea2474d507476b8ab051ebc179c20ddca35a575
John Ohno <john.ohno@gmail.com>
2022-07-20 09:40:31 -0400

    add upload script with retries and reporting

---
+++ binosite-tools/upload.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env zsh
+
+failed=""
+
+function upload() {
+	scp -r "$@" accela_miniaturebinoculars@ssh.phx.nearlyfreespeech.net:/home/public/ 
+}
+
+parts="$@"
+if [ -z $parts ]; then
+	parts=part?
+fi
+
+for i in $parts ; do
+	upload $i || (
+		echo "Upload failed for $i ; retrying (try 2)..."
+		upload $i || (
+			echo "Upload failed for $i ; retrying in 5s (try 3)..."
+			sleep 5
+			upload $i || (
+				echo "Upload failed for $i ; retrying in 1m (try 4)..."
+				sleep 1m
+				upload $i || (
+					echo "Upload failed for $i after 4 tries."
+					failed="$failed $i"
+				)
+			)
+		)
+	)
+
+[ -z $failed ] || ( echo "Failed: $failed" ; exit 1 )
