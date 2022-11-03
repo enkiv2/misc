21be3e3c071571d6f24970e074a4febf41ee575c
John Ohno <john.ohno@gmail.com>
2022-11-03 16:03:16 -0400

    implement page writing and generating header sections

--- nanogenmo-2022/XFify.py
+++ nanogenmo-2022/XFify.py
@@ -1,6 +1,57 @@
 #!/usr/bin/env python
 import sys, os
 from PIL import Image, ImageOps, ImageChops, ImageFont, ImageDraw, ImageFilter
+from random import Random
+random=Random()
+
+DPI=300
+pageSizeIn=(4.5, 7)
+
+pageSizePx=(int(pageSizeIn[0]*DPI), int(pageSizeIn[1]*DPI))
+candidates=[]
+
+def pickCandidate():
+		random.shuffle(candidates)
+		bg=None
+		for item in candidates:
+				try:
+						bg=Image.open(item).convert("RGB")
+						if bg.size:
+								return bg
+				except:
+						pass
+		return bg
+
+def newPage():
+		return Image.new("RGBA", pageSizePx, "#ffff")
+
+def layoutSectionHeader(headerName, color):
+		pages=[newPage()]
+		maxHeight=int(pageSizePx[1]/2)
+		minHeight=int(pageSizePx[1]/3)
+
+		i=0
+		while i<len(headerName):
+				height=0
+				while height<minHeight:
+						chunk=rubicate(pickCandidate(), color, headerName[i], maxHeight)
+						height=chunk.size[1]
+				i+=1
+				pages[-1].paste(chunk, (0, 0, chunk.size[0], chunk.size[1]))
+				if i<len(headerName):
+						height=0
+						while height<minHeight:
+								chunk=rubicate(pickCandidate(), color, headerName[i], maxHeight)
+								height=chunk.size[1]
+						i+=1
+						pages[-1].paste(chunk, (0, maxHeight, chunk.size[0], maxHeight+chunk.size[1]))
+						if i<len(headerName):
+								pages.append(newPage())
+		return pages
+
+def writePages(pages):
+		for i in range(0, len(pages)):
+				pages[i].save(f"page{i:05d}.png")
 
 
 # Mimic the risograph-like monochrome style of the illustrations in the XF
@@ -31,6 +82,13 @@ def rubicate(sourceImage, color, string, maxHeight=None):
 		return bottom
 
 if __name__=="__main__":
-		sourceImage=Image.open(sys.argv[1]).convert("RGB")
-		merged=rubicate(sourceImage, (255, 0, 0), "X", 200)
-		merged.save("merged.png")
+		for path in sys.argv[1:]:
+				if(os.path.isdir(path)):
+						stuff=os.walk(path)
+						for entry in stuff:
+								pfx=entry[0]
+								for sfx in entry[2]:
+										candidates.append(pfx+"/"+sfx)
+				else:
+						candidates.append(path)
+		writePages(layoutSectionHeader("SPECTACLE", (255, 0, 0)))
