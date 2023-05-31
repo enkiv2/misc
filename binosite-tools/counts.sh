353572bb46f325ce6fe99a3cbfc838ee8d5c2804
John Ohno <john.ohno@gmail.com>
2023-05-31 09:46:26 -0400

    add to obliqueprog from https://merveilles.town/@zens/110132016932073022

--- binosite-tools/counts.sh
+++ binosite-tools/counts.sh
@@ -7,3 +7,5 @@ scp accela_miniaturebinoculars@ssh.phx.nearlyfreespeech.net:/home/logs/access_lo
 echo "Count: $(grepcount '\.htm')"
 echo "PDF count: $(grepcount '\.pdf')"
 echo "JB/JE manufacturer code list count: $(grepcount 'Page289\.htm')"
+echo "Other binoculars count: $(for i in 21222 2762 3047 3260 3540 3595 4610 3090 4508 290 ; do grepcount Page${i}.htm ; done | awk '{x+=$0} END{print(x)}')"
+
