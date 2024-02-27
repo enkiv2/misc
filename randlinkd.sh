87d944554248080f0ab826f3ae7e7ff1b5717b7b
John Ohno <john.ohno@gmail.com>
2024-02-27 10:31:33 -0500

    decrease max message size for bluesky

--- randlinkd.sh
+++ randlinkd.sh
@@ -1,3 +1,5 @@
+MAX=300
+
 processLink() {
 	read item;
 	date="$(echo "$item" | cut -f 2)" 
@@ -6,15 +8,15 @@ processLink() {
 	url2="https://web.archive.org/web/$date2/$url"
 	title="$(echo "$item" | cut -f 3-)"
 	template="Random link from the archives: \"$title\" $url2 originally retrieved $date"
-	if [ $(echo "$template" | wc -c) -gt 500 ]; then
+	if [ $(echo "$template" | wc -c) -gt $MAX ]; then
 		template="Random link from the archives: \"$title\" $url2"
-		if [$(echo "$template" | wc -c) -gt 500 ]; then
+		if [$(echo "$template" | wc -c) -gt $MAX ]; then
 			template="#randomArchiveLink \"$title\" $url2 @ $date"
-			if [$(echo "$template" | wc -c) -gt 500 ]; then
+			if [$(echo "$template" | wc -c) -gt $MAX ]; then
 				template="#randomArchiveLink \"$title\" $url2 @ $date2"
-				if [$(echo "$template" | wc -c) -gt 500 ]; then
+				if [$(echo "$template" | wc -c) -gt $MAX ]; then
 					template="#randomArchiveLink \"$title\" $url2"
-					if [$(echo "$template" | wc -c) -gt 500 ]; then
+					if [$(echo "$template" | wc -c) -gt $MAX ]; then
 						template="#randomArchiveLink $url2 @ $date"
 					fi
 				fi
@@ -27,5 +29,5 @@ processLink() {
 	post "$(shuf -n 1 ~/.linkit | processLink)" 
 	sleep 4h
 	sleep 23m
-done 2>&1) > /dev/null 
+done 2>&1)  > /dev/null 
 
