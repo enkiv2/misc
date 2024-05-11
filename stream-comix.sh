d629cf7449205a48a874745a2ecd4ebde1c55a9e
John Ohno <john.ohno@gmail.com>
2024-05-11 02:10:49 -0400

    clean up and support more types

--- stream-comix.sh
+++ stream-comix.sh
@@ -5,31 +5,69 @@ delay=$1 ; shift
 
 extdir=~/.stream-comix/$$/
 
+function recurse_stream() {
+	mkdir -p $extdir
+	type=$1 ; shift
+
+	echo -e "=====\n\ttype: $type\n\tfilename: $@\n\textdir: $extdir\n=====\n"
+
+	case $type in
+		zip)
+			unzip "$@" -d "$extdir"
+		;;
+		rar)
+			unrar -x "$@" "$extdir"
+		;;
+		pdf)
+			convert "$@" "$extdir/%3d.jpg"
+		;;
+		html)
+			pandoc "$@" "$extdir/out.pdf"
+		;;
+		iso)
+			fuseiso "$@" "$extdir"
+			$cmd $delay $extdir
+			umount "$extdir"
+		;;
+		mp4)
+			pushd "$extdir"
+			mplayer -ao null -vo jpeg	"$@"
+			popd
+		;;
+		*)
+			feh --bg-max "$@" &&
+			feh --bg-tile ~/Downloads/dingir.jpg &&
+			feh --bg-max "$@" && sleep $delay
+			return
+		;;
+	esac
+	$cmd $delay $extdir
+	rm -rf $extdir
+}
+
+rules='
+				s/\.cbz$/.zip/;
+				s/\.cbr$/.rar/;
+				s/\.xhtml$/.html/;
+				s/\.htm$/.html/;
+				s/\.xhtm$/.html/;
+				s/\.doc$/.html/;
+				s/\.docx$/.html/;
+				s/\.epub$/.html/;
+				s/\.mobi$/.html/;
+				s/\.txt$/.html/;
+				s/\.md$/.html/;
+				s/\.ppt$/.html/;
+				s/\.pttx$/.html/;
+				s/\.\(wmv|mpeg|avi|mkv|mov|mpg|webm\)$/.mp4/'
+
 (for dir in "$@" ; do
 	echo "Showing $dir"
 	find $dir -type f
 done) | sort | while read x ; do 
 	echo "file: $x"
-	if echo "$x" | egrep -iq '\.(cbz|zip|epub)$' ; then
-		mkdir -p $extdir
-		unzip "$x" -d $extdir
-		$cmd $delay $extdir
-		rm -rf $extdir
-	elif echo "$x" | egrep -iq '\.(cbr|rar)$' ; then
-		mkdir -p $extdir
-		unrar -x "$x" $extdir
-		$cmd $delay $extdir
-		rm -rf $extdir
-	elif echo "$x" | egrep -iq '\.(pdf)$' ; then 
-		mkdir -p $extdir
-		convert "$x" $extdir
-		$cmd $delay $extdir
-		rm -rf $extdir
-	else	
-		feh --bg-max "$x" &&
-		feh --bg-tile ~/Downloads/dingir.jpg &&
-		feh --bg-max "$x" && sleep $delay
-	fi
+	type=$(echo "$x" | sed "$rules" | tr 'A-Z' 'a-z' | sed 's/^.*\.\([^.]*\)$/\1/')
+	recurse_stream "$type" "$x"
 done
 
 		
