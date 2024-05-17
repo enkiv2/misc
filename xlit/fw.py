95db21a0008c6f99733222cea5b4121577da09d8
John Ohno <john.ohno@gmail.com>
2024-05-17 13:50:00 -0400

    start working on beams (but coordinate math is annoying af)

--- xlit/fw.py
+++ xlit/fw.py
@@ -32,8 +32,14 @@ class Vector:
 		for i in range(0, n):
 		  ret[i]=self.coords[i]-o.coords[i]
 		return ret
+  def __mul__(self, o)
+	  n=min(self.n, len(o))
+	  ret=[0]*n
+		for i in range(0, n):
+		  ret[i]=self.coords[i]*o.coords[i]
+		return ret
 
-ORIGIN = Vector((0, 0, 0))
+ORIGIN = Vector((0, 0))
 
 class FWObj:
   PROPERTY_DEFAULTS = {
@@ -57,12 +63,15 @@ class FWObj:
 	  if name in defaults:
 		  return self.getAttr(name, defaults[name])
 		raise AttributeError()
+  def _render(self, screen):
+	  pass
   def render(self, screen):
+	  self._render(screen)
 	  for child in self.children:
 		  child.render(screen)
   @property
   def bbox(self):
-	  return [self.pos, self.pos+self.size]
+	  return [self.pos-(self.size*self.center), self.pos+(self.size*self.center)]
   @property
 	def children(self):
 	  cells=[]
@@ -77,10 +86,8 @@ class FWObj:
   @property
   def pos(self):
     parent=self.parent
-		coords=[0, 0, 0]
-		for i in range(0, 3):
-      coords[i]=parent.pos[i]+(parent.size[i]*(parent.center[i]-self.parentcenter[i]))+self.delta[i]-(self.size[i]*self.center[i])
- 
+    return parent.pos+(parent.size*(parent.center-self.parentcenter))+self.delta-(self.size*self.center)
+
 class Slab(FWObj):
   PROPERTY_DEFAULTS = {
 	  "type": "slab",
@@ -88,11 +95,36 @@ class Slab(FWObj):
 		"center": ORIGIN,
 		"parentcenter": ORIGIN,
 		"delta": ORIGIN,
-		"color": (255, 255, 255, 127)
+		"color": (255, 255, 255, 127),
+		"outline_width": 0,
+		"border_radius": 0,
 	}
-  def render(self, screen):
-	  pygame.draw.rect(screen, self.color, pygame.Rect(self.bbox))
-	  for child in self.children:
-		  child.render(screen)
+  def _render(self, screen):
+	  pygame.draw.rect(screen, self.color, pygame.Rect(self.bbox), self.outline_width, self.border_radius)
 
+class Beam(FWObj):
+  PROPERTY_DEFAULTS = {
+	  "type": "beam",
+		"size": ORIGIN,
+		"center": ORIGIN,
+		"parentcenter": ORIGIN,
+		"delta": ORIGIN,
+		"color": (127, 255, 127, 127),
+		"endsets": [],
+		"bookmark_width": 5,
+	}
+  def _render(self, screen):
+	  endset_bboxes=[]
+		for endset in self.endsets:
+		  endset_bboxes.append(endset.bbox)
+		coords = []
+		if len(endset_bboxes) == 1:
+		  end = ((bbox[1][1], bbox[0][0]), (bbox[1][1], bbox[1][1]))
+		  coords = [end[0], (Vector(end[0])+Vector([self.bookmark_width, self.bookmark_width])).coords, (Vector(end[1]+Vector([self.bookmark_width, self.bookmark_width])).coords, end[1]]
+		elif len(endset_bboxes) == 2:
+		  (first, second) = (endset_bboxes[0], endset_bboxes[1])
+			if second[0][0]<first[1][0]:
+			  (first, second) = (second, first)
+		  coords = [(first[1][0], first[1][1]), (
+	  pygame.draw.polygon(screen, self.color, coords)
 
