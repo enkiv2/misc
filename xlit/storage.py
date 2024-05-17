f25360a9de11d1e24a710801610cb77fd92ffaf0
John Ohno <john.ohno@gmail.com>
2024-05-17 11:27:15 -0400

    indentation two spaces, because python3 hates tabs

--- xlit/storage.py
+++ xlit/storage.py
@@ -4,26 +4,26 @@ import ipfsApi
 ipfs = ipfsApi.Client("127.0.0.1", 5801)
 
 def fetch(triples):
-		chunks=[]
-		for (cid, start, length) in triples:
-				chunks.append(fetchSingle(cid, start, length))
-		return chunks
+  chunks=[]
+  for (cid, start, length) in triples:
+    chunks.append(fetchSingle(cid, start, length))
+  return chunks
 
 
 # TODO: garbage collection or garbage compaction with pins:
 # keep database of when things were pinned, source, and reason
 # and periodically unpin (but do not remove) the oldest that were just cached
 def fetchSingle(cid, start=0, length=-1, pin: bool = False):
-		ret = ipfs.get_pyobj(cid)[start][:length]
-		if pin:
-				ipfs.pin.add(cid)
-		return ret
+  ret = ipfs.get_pyobj(cid)[start][:length]
+  if pin:
+    ipfs.pin.add(cid)
+  return ret
 
 def store(data: bytes, pin: bool = True):
-		cid = ipfs.add_pyobj(data)
-		if pin:
-				ipfs.pin.add(cid)
-		return ret
+  cid = ipfs.add_pyobj(data)
+  if pin:
+    ipfs.pin.add(cid)
+  return ret
 
 # TODO: permascroll management
 # private/working permascroll is stored locally, append-only
